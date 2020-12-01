d867da640268bd353c430a336a350173032ffdec
Kai Schnackenberg <kai.schnackenberg@rewe-digital.com>
2020-12-01 08:39:13 +0100

    Fix .gitignore and refactor Day1

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,9 @@ import Day1(part1, part2)
 
 main :: IO ()
 main = do
-    res1_1 <- Day1.part1 "../input/Day1.txt"
+    putStrLn "Day 1 - Part 1:"
+    res1_1 <- Day1.part1 "input/Day1.txt"
     print res1_1
-    res1_2 <- Day1.part2 "../input/Day1.txt"
+    putStrLn "Day 1 - Part 2:"
+    res1_2 <- Day1.part2 "input/Day1.txt"
     print res1_2
--- src/Day1.hs
+++ src/Day1.hs
@@ -1,24 +1,25 @@
-module Day1(
-  part1,
-  part2,
-  ) where
+module Day1(part1, part2) where
 
 import Data.List(find)
 
 solvePart1 :: Int -> [Int] -> Maybe Int
 solvePart1 _ [] = Nothing
-solvePart1 res (x:xs) = case find (\i -> i + x == res) xs of
-  Just f -> Just $ f * x
-  Nothing -> solvePart1 res xs
+solvePart1 expected (x:xs) = ((* x) <$> find (\i -> i + x == expected) xs) `orIfNothing` solvePart1 expected xs
 
 solvePart2 :: Int -> [Int] -> Maybe Int
 solvePart2 _ [] = Nothing
-solvePart2 res (x:xs) = case solvePart1 (res - x) xs of
-  Just f -> Just $ f * x
-  Nothing -> solvePart2 res xs
+solvePart2 expected (x:xs) = ((* x) <$> solvePart1 (expected - x) xs) `orIfNothing` solvePart2 expected xs
+
+-- There has to be an existing function for this?
+orIfNothing:: Maybe a -> Maybe a -> Maybe a
+orIfNothing Nothing b = b
+orIfNothing (Just a) _ = Just a
+
+readInputFile :: FilePath -> IO [Int]
+readInputFile file = fmap read . lines <$> readFile file
 
 part1 :: FilePath -> IO (Maybe Int)
-part1 file = solvePart1 2020 . (fmap read . lines) <$> readFile file
+part1 file = solvePart1 2020 <$> readInputFile file
 
 part2 :: FilePath -> IO (Maybe Int)
-part2 file = solvePart2 2020 . (fmap read . lines) <$> readFile file
+part2 file = solvePart2 2020 <$> readInputFile file
