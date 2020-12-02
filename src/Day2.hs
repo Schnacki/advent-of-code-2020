95dc9183308fec0ca9a4cf2049bc83591ab17070
Kai Schnackenberg <kai.schnackenberg@rewe-digital.com>
2020-12-02 07:24:41 +0100

    refactor and fix test

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,29 +1,26 @@
-module Day2(solvePart1, Password(Password), part1, part2) where
+module Day2(solvePart1, solvePart2, Password(Password), part1, part2) where
 
 import Data.Ix(inRange)
 import Data.List.Split
 
 data Password = Password (Int,Int) Char String deriving Show
 
-passwordValid1 :: Password -> Bool
-passwordValid1 (Password range letter password) = inRange range . length . filter (== letter) $ password
-
 solvePart1 :: [Password] -> Int
-solvePart1 = length . filter passwordValid1
-
-passwordValid2 :: Password -> Bool
-passwordValid2 (Password (a, b) letter password) = (password !! (a - 1) == letter) /= (password !! (b - 1) == letter) 
+solvePart1 = length . filter valid
+    where valid (Password range letter password) = inRange range . length . filter (== letter) $ password
 
 solvePart2 :: [Password] -> Int
-solvePart2 = length . filter passwordValid2
+solvePart2 = length . filter valid
+    where valid (Password (a, b) letter password) = (password !! (a - 1) == letter) /= (password !! (b - 1) == letter) 
 
-readPassword :: String -> Password
-readPassword input = let 
-    (min:max:letter:_:password:_) = splitOneOf "-: " input 
-    in Password (read min, read max) (head letter) password
+readPasswordFile :: FilePath -> IO [Password]
+readPasswordFile file = fmap readPassword . lines <$> readFile file
+    where readPassword input = let 
+            (min:max:letter:_:password:_) = splitOneOf "-: " input 
+            in Password (read min, read max) (head letter) password
     
 part1 :: FilePath -> IO Int
-part1 file = solvePart1 . fmap readPassword . lines <$> readFile file
+part1 file = solvePart1 <$> readPasswordFile file
 
 part2 :: FilePath -> IO Int
-part2 file = solvePart2 . fmap readPassword . lines <$> readFile file
+part2 file = solvePart2 <$> readPasswordFile file
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,7 +2,7 @@ import Test.Hspec
 import Test.QuickCheck
 
 import Day1(solvePart1, solvePart2)
-import Day2(solvePart1, Password(Password))
+import Day2(solvePart1, solvePart2, Password(Password))
 
 main :: IO ()
 main = hspec $ do
@@ -18,4 +18,4 @@ main = hspec $ do
       Day2.solvePart1 [Password (1, 3) 'a' "abcde", Password (1, 3) 'b' "cdefg", Password (2, 9) 'c' "ccccccccc"] `shouldBe` 2
 
     it "The second example passes!" $ do
-      Day2.solvePart1 [Password (1, 3) 'a' "abcde", Password (1, 3) 'b' "cdefg", Password (2, 9) 'c' "ccccccccc"] `shouldBe` 1
+      Day2.solvePart2 [Password (1, 3) 'a' "abcde", Password (1, 3) 'b' "cdefg", Password (2, 9) 'c' "ccccccccc"] `shouldBe` 1
