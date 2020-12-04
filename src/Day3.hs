82266062b26ebf7baca0dfa0850ac7787881a9cd
Kai Schnackenberg <kai.schnackenberg@rewe-digital.com>
2020-12-04 16:29:14 +0100

    Solve Day 3

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,8 +1,25 @@
-module Day3(part1, part2) where
+module Day3(part1, part2, solvePart1, solvePart2) where
 
+type Area = [[Char]]
+type Route = [(Int, Int)]
+
+route :: (Int, Int) -> (Int, Int) -> Route
+route (width, height) (x, y) = takeWhile (\(a, b) -> b < height) $ zipWith (\a-> \b-> (mod a width, b)) [x, 2 * x ..] [y, 2 * y ..]
+
+dimension :: Area -> (Int, Int)
+dimension area =  (length $ head area, length area)
+
+solveSlope :: Area -> (Int, Int) -> Int
+solveSlope area slope = length . filter (== '#') . map (\(x, y) -> (area !! y) !! x) $ (route (dimension area) slope)
+
+solvePart1 :: Area -> Int
+solvePart1 area = solveSlope area (3, 1)
+
+solvePart2 :: Area -> Int
+solvePart2 area = product $ map (solveSlope area) [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 
 part1 :: FilePath -> IO ()
-part1 file = return ()
+part1 file = readFile file >>= ( print . solvePart1 . lines)
 
 part2 :: FilePath -> IO ()
-part2 file = return ()
+part2 file = readFile file >>= ( print . solvePart2 . lines)
--- test/Day3Spec.hs
+++ test/Day3Spec.hs
@@ -3,7 +3,24 @@ module Day3Spec(spec) where
 import Test.Hspec
 import Day3
 
-spec = do
-  describe "Part 1:" $ return ()
+area = [
+  "..##.......",
+  "#...#...#..",
+  ".#....#..#.",
+  "..#.#...#.#",
+  ".#...##..#.",
+  "..#.##.....",
+  ".#.#.#....#",
+  ".#........#",
+  "#.##...#...",
+  "#...##....#",
+  ".#..#...#.#"]
 
-  describe "Part 2:" $ return ()
+spec = do
+  describe "Part 1:" $
+    it "There are 7 trees in the (3, 1) - slope!" $ do
+      Day3.solvePart1 area `shouldBe` 7
+
+  describe "Part 2:" $
+    it "The trees in the (1, 1), (3, 1), (5, 1), (7, 1), (1, 2) - slopes multiply to 336!" $ do
+      Day3.solvePart2 area `shouldBe` 336
