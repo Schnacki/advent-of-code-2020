4bc16c051b99a1e0d29ec20c73ed1f6e341a6f09
Kai Schnackenberg <kai.schnackenberg@rewe-digital.com>
2020-12-05 08:53:06 +0100

    refactor Day4, tests are failing right now

--- src/Day4.hs
+++ src/Day4.hs
@@ -10,20 +10,18 @@ import Text.Read (readMaybe)
 data Passport = Passport { byr :: Int, iyr :: Int, eyr :: Int, hgt :: String, hcl :: String, ecl :: String, pid :: String } deriving Show
 
 parsePassport :: [(String, String)] -> Maybe Passport
-parsePassport pwd = do
-  byr <- lookup "byr" pwd >>= readMaybe
-  iyr <- lookup "iyr" pwd >>= readMaybe
-  eyr <- lookup "eyr" pwd >>= readMaybe
-  hgt <- lookup "hgt" pwd
-  hcl <- lookup "hcl" pwd
-  ecl <- lookup "ecl" pwd
-  pid <- lookup "pid" pwd
+parsePassport p = do
+  byr <- lookup "byr" p >>= readMaybe
+  iyr <- lookup "iyr" p >>= readMaybe
+  eyr <- lookup "eyr" p >>= readMaybe
+  hgt <- lookup "hgt" p
+  hcl <- lookup "hcl" p
+  ecl <- lookup "ecl" p
+  pid <- lookup "pid" p
   return $ Passport byr iyr eyr hgt hcl ecl pid
 
 parseInput :: String -> [Maybe Passport]
-parseInput = map (parsePassport . toTuples . words . unwords) . splitWhen (== "") . lines
-  where
-    toTuples = map (second tail . span (/= ':'))
+parseInput = map (parsePassport . map (second tail . span (/= ':')) . words . unwords) . splitWhen (== "") . lines
 
 solvePart1 :: String -> Int
 solvePart1 = length . catMaybes . parseInput
@@ -33,36 +31,36 @@ part1 file = readFile file >>= print . solvePart1
 
 
 passportIdValid :: Passport -> Bool
-passportIdValid pwd = length (pid pwd) == 9 && all isDigit (pid pwd)
+passportIdValid p = length (pid p) == 9 && all isDigit (pid p)
 
 eyeColorValid :: Passport -> Bool
-eyeColorValid pwd = (ecl pwd) `elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+eyeColorValid p = (ecl p) `elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
 
 hairColorValid :: Passport -> Bool
-hairColorValid pwd = let (hash : number) = hcl pwd in
+hairColorValid p = let (hash : number) = hcl p in
   hash == '#' && length number == 6 && all isHexDigit number
 
 birthYearValid :: Passport -> Bool
-birthYearValid pwd = (byr pwd) >= 1920 && (byr pwd) <= 2002
+birthYearValid p = (byr p) >= 1920 && (byr p) <= 2002
 
 issuerYearValid :: Passport -> Bool
-issuerYearValid pwd = (iyr pwd) >= 2010 && (iyr pwd) <= 2020
+issuerYearValid p = (iyr p) >= 2010 && (iyr p) <= 2020
 
 expirationYearValid :: Passport -> Bool
-expirationYearValid pwd = (eyr pwd) >= 2020 && (eyr pwd) <= 2030
+expirationYearValid p = (eyr p) >= 2020 && (eyr p) <= 2030
 
 heightValid :: Passport -> Bool
-heightValid pwd = let (height, unit) = span isDigit (hgt pwd)
+heightValid p = let (height, unit) = span isDigit (hgt p)
    in case unit of
         "cm" -> (read height :: Int) >= 150 && (read height :: Int) <= 193
         "in" -> (read height :: Int) >= 59 && (read height :: Int) <= 76
         _ -> False
 
-passportDataValid2 :: Passport -> Bool
-passportDataValid2 pwd = and $ [passportIdValid, eyeColorValid, hairColorValid, birthYearValid, issuerYearValid, expirationYearValid, heightValid] <*> [pwd]
+passportDataValid :: Passport -> Bool
+passportDataValid p = and $ [passportIdValid, eyeColorValid, hairColorValid, birthYearValid, issuerYearValid, expirationYearValid, heightValid] <*> [p]
 
 solvePart2 :: [Passport] -> Int
-solvePart2 = length . filter passportDataValid2
+solvePart2 = length . filter passportDataValid
 
 part2 :: FilePath -> IO ()
 part2 file = readFile file >>= print . solvePart2 . catMaybes . parseInput
