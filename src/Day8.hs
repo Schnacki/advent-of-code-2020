d2f5c0f16b656b481a1949b36271106d82d59f60
Kai Schnackenberg <kai.schnackenberg@rewe-digital.com>
2020-12-08 12:43:17 +0100

    Refactor

--- src/Day8.hs
+++ src/Day8.hs
@@ -6,14 +6,26 @@ data Operation = NOP Int | ACC Int | JMP Int deriving (Show, Eq)
 
 data Computation = Computation [Int] Int Int [Operation]
 
+parseInput :: String -> Computation
+parseInput str = Computation [] 0 0 (map parseLine . lines $ str)
+  where
+    parseLine ('n' : 'o' : 'p' : ' ' : number) = NOP $ read (filter (/= '+') number)
+    parseLine ('a' : 'c' : 'c' : ' ' : number) = ACC $ read (filter (/= '+') number)
+    parseLine ('j' : 'm' : 'p' : ' ' : number) = JMP $ read (filter (/= '+') number)
+
 performOperation :: Computation -> Computation
 performOperation (Computation seen index val ops) = case ops !! index of
   NOP _ -> Computation (index : seen) (index + 1) val ops
   ACC a -> Computation (index : seen) (index + 1) (val + a) ops
   JMP j -> Computation (index : seen) (index + j) val ops
 
-flipOperations :: [Operation] -> [[Operation]]
-flipOperations = flipOps []
+solvePart1 :: Computation -> Int
+solvePart1 comp@(Computation seen index val ops)
+  | index `elem` seen = val
+  | otherwise = solvePart1 . performOperation $ comp
+
+corruptComputation :: Computation -> [Computation]
+corruptComputation (Computation seen index val ops) = fmap (Computation seen index val) (flipOps [] ops)
   where
     mayFlip (ACC _) = False
     mayFlip _ = True
@@ -23,25 +35,13 @@ flipOperations = flipOps []
     flipOps h [] = []
     flipOps h (t : ts) = let ops = flipOps (h ++ [t]) ts in if mayFlip t then (h ++ flipOp t : ts) : ops else ops
 
-parseInput :: String -> Computation
-parseInput str = Computation [] 0 0 (map parseLine . lines $ str)
-  where
-    parseLine ('n' : 'o' : 'p' : ' ' : number) = NOP $ read (filter (/= '+') number)
-    parseLine ('a' : 'c' : 'c' : ' ' : number) = ACC $ read (filter (/= '+') number)
-    parseLine ('j' : 'm' : 'p' : ' ' : number) = JMP $ read (filter (/= '+') number)
-
-solvePart1 :: Computation -> Int
-solvePart1 comp@(Computation seen index val ops)
-  | index `elem` seen = val
-  | otherwise = solvePart1 $ performOperation comp
-
 solvePart2 :: Computation -> Int
-solvePart2 (Computation seen index val ops) = head . mapMaybe (solve . Computation seen index val) . flipOperations $ ops
+solvePart2 = head . mapMaybe solve . corruptComputation
   where
     solve comp@(Computation seen index val ops)
       | index `elem` seen = Nothing
       | index >= length ops = Just val
-      | otherwise = solve $ performOperation comp
+      | otherwise = solve . performOperation $ comp
 
 part1 :: FilePath -> IO ()
 part1 file = print . solvePart1 . parseInput =<< readFile file
