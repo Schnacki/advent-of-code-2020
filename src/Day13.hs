732a8bde78a9a55a28e899ff4a8d3fa4dffdc0ec
Kai Schnackenberg <kai.schnackenberg@rewe-digital.com>
2020-12-13 16:14:50 +0100

    Solve day 13 part 2

--- src/Day13.hs
+++ src/Day13.hs
@@ -1,6 +1,7 @@
-module Day13 (part1, part2, solvePart1, parseInput) where
+module Day13 (part1, part2, solvePart1, solvePart2, parseInput) where
 
 import Data.List.Split (splitOn)
+import Data.Maybe  (fromJust)
 
 import Math.NumberTheory.Moduli.Chinese
 
@@ -19,6 +20,16 @@ solvePart1 firstDeparture = calculateResult . minimum . map (\t -> head $ dropWh
 part1 :: String -> Int
 part1 = uncurry Day13.solvePart1 . parseInput
 
+parseInput2 :: String -> [(Integer, Integer)]
+parseInput2 = map (\(a,b) -> (a, read b)) . filter ((/= "x") . snd) . zip [0 .. ] . splitOn "," . last . lines
+
+solvePart2 :: [(Integer, Integer)] -> Integer
+solvePart2 = fromJust . chineseRemainder . map (\(a,b) -> ((b - a) `mod` b,b))
+
+part2 :: String -> Integer
+part2 = solvePart2 . parseInput2
+
+
 {-
 Equations to solve:
 
@@ -33,6 +44,3 @@ t + 58 mod 41  = 0 <=> t mod 41  = 24
 7 + 67 mod 19  = 0 <=> t mod 19  = 9
 
 -}
-
-part2 :: String -> Integer
-part2 str = let Just res = chineseRemainder [(0,17),(26,37),(422,439),(10,29),(9,13),(6,23),(739,787),(24,41),(9,19)] in res
--- test/Day13Spec.hs
+++ test/Day13Spec.hs
@@ -7,3 +7,12 @@ spec = do
   describe "Part 1:" $ do
     it "939 [7,13,59,31,19] results in 295." $ do
       Day13.solvePart1 939 [7,13,59,31,19] `shouldBe` 295
+  describe "Part 2:" $ do
+    it "The earliest timestamp that matches the list 17,x,13,19 is 3417." $ do
+      Day13.solvePart2 [(0,17),(2,13),(3,19)] `shouldBe` 3417
+    it "67,7,59,61 first occurs at timestamp 754018." $ do
+      Day13.solvePart2 [(0,67),(1,7),(2,59),(3,61)] `shouldBe` 754018
+    it "67,x,7,59,61 first occurs at timestamp 779210." $ do
+      Day13.solvePart2 [(0,67),(2,7),(3,59),(4,61)] `shouldBe` 779210
+    it "67,7,x,59,61 first occurs at timestamp 1261476." $ do
+      Day13.solvePart2 [(0,67),(1,7),(3,59),(4,61)] `shouldBe` 1261476
