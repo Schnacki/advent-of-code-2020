99dc6c71944be1ba42e88464d4e558abf4599f28
Kai Schnackenberg <kai.schnackenberg@rewe-digital.com>
2020-12-14 18:40:03 +0100

    refactor

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,4 +1,4 @@
-module Day14 (part1, part2, parseInput, Input(..), solvePart1, applyMask) where
+module Day14 (part1, part2, Input (..), solvePart1) where
 
 import Data.Bits
 import qualified Data.Map as Map
@@ -9,7 +9,7 @@ import Text.Megaparsec.Char.Lexer as L
 
 type Parser = Parsec Void String
 
-data Input = Mask String | Memory Int Int deriving Show
+data Input = Mask String | Mem Int Int deriving (Show)
 
 parseMask :: Parser Input
 parseMask = do
@@ -25,29 +25,31 @@ parseMem = do
   _ <- string "] = "
   value <- L.decimal
   _ <- optional $ char '\n'
-  return $ Memory addr value
+  return $ Mem addr value
 
-inputParser = M.many $ choice [parseMask,parseMem]
+inputParser = M.many $ choice [parseMask, parseMem]
 
 parseInput :: String -> [Input]
 parseInput input = case M.parse inputParser "" input of
-              Left _ -> []
-              Right a -> a
+  Left _ -> []
+  Right a -> a
 
 applyMask :: String -> Int -> Int
 applyMask mask value = value .&. andMask .|. orMask
   where
     andMask :: Int
-    andMask = complement . foldl (\b a -> 2*b + if a == '0' then 1 else 0) 0 $ mask
-    orMask = foldl (\b a -> 2*b + if a == '1' then 1 else 0) 0 mask
+    andMask = complement . foldl (\b a -> 2 * b + if a == '0' then 1 else 0) 0 $ mask
+    orMask = foldl (\b a -> 2 * b + if a == '1' then 1 else 0) 0 mask
 
-solvePart1 :: [Input] -> Map.Map Int Int -> String -> Int
-solvePart1 [] map _ = Map.foldr (+) 0 map
-solvePart1 ((Mask newMask):xs) map _ = solvePart1 xs map newMask
-solvePart1 ((Memory addr value):xs) map mask = solvePart1 xs (Map.alter (\_ -> Just (applyMask mask value)) addr map) mask
+solvePart1 :: [Input] -> Int
+solvePart1 input = solve input Map.empty ""
+  where
+    solve [] map _ = Map.foldr (+) 0 map
+    solve ((Mask newMask) : xs) map _ = solve xs map newMask
+    solve ((Mem addr value) : xs) map mask = solve xs (Map.alter (\_ -> Just (applyMask mask value)) addr map) mask
 
 part1 :: String -> Int
-part1 = (\input -> solvePart1 input Map.empty "") . parseInput
+part1 = solvePart1 . parseInput
 
 part2 :: String -> Int
 part2 str = 0
