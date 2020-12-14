9fcee338f7c24057c1114167958f61504c0ca3e1
Kai Schnackenberg <kai.schnackenberg@rewe-digital.com>
2020-12-14 23:14:56 +0100

    solve day 14 - part 2

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,4 +1,4 @@
-module Day14 (part1, part2, Input (..), solvePart1) where
+module Day14 (part1, part2, Input (..), solvePart1, solvePart2, parseInput,BitOperation(..),applyMask', parseMask') where
 
 import Data.Bits
 import qualified Data.Map as Map
@@ -13,43 +13,61 @@ data Input = Mask String | Mem Int Int deriving (Show)
 
 parseMask :: Parser Input
 parseMask = do
-  _ <- string "mask = "
+  string "mask = "
   mask <- M.some alphaNumChar
-  _ <- optional $ char '\n'
+  optional $ char '\n'
   return $ Mask mask
 
 parseMem :: Parser Input
 parseMem = do
-  _ <- string "mem["
+  string "mem["
   addr <- L.decimal
-  _ <- string "] = "
+  string "] = "
   value <- L.decimal
-  _ <- optional $ char '\n'
+  optional $ char '\n'
   return $ Mem addr value
 
-inputParser = M.many $ choice [parseMask, parseMem]
-
 parseInput :: String -> [Input]
-parseInput input = case M.parse inputParser "" input of
+parseInput input = case M.parse (M.many $ choice [parseMask, parseMem]) "" input of
   Left _ -> []
   Right a -> a
 
 applyMask :: String -> Int -> Int
 applyMask mask value = value .&. andMask .|. orMask
   where
-    andMask :: Int
     andMask = complement . foldl (\b a -> 2 * b + if a == '0' then 1 else 0) 0 $ mask
     orMask = foldl (\b a -> 2 * b + if a == '1' then 1 else 0) 0 mask
 
 solvePart1 :: [Input] -> Int
 solvePart1 input = solve input Map.empty ""
   where
-    solve [] map _ = Map.foldr (+) 0 map
-    solve ((Mask newMask) : xs) map _ = solve xs map newMask
-    solve ((Mem addr value) : xs) map mask = solve xs (Map.alter (\_ -> Just (applyMask mask value)) addr map) mask
+    solve [] mem _ = Map.foldr (+) 0 mem
+    solve ((Mask newMask) : xs) mem _ = solve xs mem newMask
+    solve ((Mem addr value) : xs) mem mask = solve xs (Map.insert addr (applyMask mask value) mem) mask
 
 part1 :: String -> Int
 part1 = solvePart1 . parseInput
 
+
+data BitOperation = Set | Clear | Floating deriving Show
+type BitModification = (Int,BitOperation)
+
+parseMask' :: String -> [BitModification]
+parseMask' = map (\(b,i) -> if i == '1' then (b,Set) else (b,Floating)) . filter (\(_,i) -> i /= '0') . zip [35, 34 ..0]
+
+applyMask' :: [BitModification] -> Int -> [Int]
+applyMask' [] value = [value]
+applyMask' ((i,Set):ops) value = map (\v -> setBit v i) (applyMask' ops value)
+applyMask' ((i,Clear):ops) value = map (\v -> clearBit v i) (applyMask' ops value)
+applyMask' ((i,Floating):ops) value= (applyMask' ops value) >>= ((\v -> [clearBit v i,setBit v i]))
+
+
+solvePart2 :: [Input] -> Int
+solvePart2 input = solve input Map.empty ""
+  where
+    solve [] mem _ = Map.foldr (+) 0 mem
+    solve ((Mask newMask) : xs) mem _ = solve xs mem newMask
+    solve ((Mem addr value) : xs) mem mask = solve xs (foldr (\a map -> Map.insert a value map) mem  (applyMask' (parseMask' mask) addr)) mask
+
 part2 :: String -> Int
-part2 str = 0
+part2 = solvePart2 . parseInput
--- test/Day14Spec.hs
+++ test/Day14Spec.hs
@@ -4,9 +4,14 @@ import Day14
 import Test.Hspec
 import qualified Data.Map as M
 
-input = [Day14.Mask "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", Day14.Memory 8 11, Day14.Memory 7 101, Day14.Memory 8 0]
+input = [Mask "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", Mem 8 11, Mem 7 101, Mem 8 0]
+
+input2 = [Mask "000000000000000000000000000000X1001X", Mem 42 100, Mask "00000000000000000000000000000000X0XX", Mem 26 1]
 
 spec = do
   describe "Part 1:" $ do
     it "The memory sums up to 165." $ do
-      Day14.solvePart1 input M.empty "" `shouldBe` 165
+      solvePart1 input `shouldBe` 165
+  describe "Part 2:" $ do
+    it "The memory sums up to 208." $ do
+      solvePart2 input2 `shouldBe` 208
