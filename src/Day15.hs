4f5cb03aa3fe684eb2678b742d45546ba14381a0
Kai Schnackenberg <kai.schnackenberg@rewe-digital.com>
2020-12-15 23:57:07 +0100

    use int map and int instead of map and integer

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,20 +1,20 @@
 module Day15 (part1, part2) where
 
 import Data.List.Split (splitOn)
-import qualified Data.Map as M
+import qualified Data.IntMap as M
 import Data.Maybe (maybe)
 
-solve :: Integer -> Integer -> Integer -> M.Map Integer Integer -> Integer
+solve :: Int -> Int -> Int -> M.IntMap Int -> Int
 solve limit index number map = if index == limit then number else solve limit (index + 1) (maybe 0 (index -) (M.lookup number map)) (M.insert number index map)
 
-solvePart :: Integer -> M.Map Integer Integer -> Integer
-solvePart limit map = solve limit ((toInteger $ M.size map) + 1) 0 map
+solvePart :: Int -> M.IntMap Int -> Int
+solvePart limit map = solve limit (1 + M.size map) 0 map
 
-parseInput :: String -> M.Map Integer Integer
+parseInput :: String -> M.IntMap Int
 parseInput = M.fromList . (flip zip) [1 ..] . fmap read . splitOn ","
 
-part1 :: String -> Integer
+part1 :: String -> Int
 part1 = solvePart 2020 . parseInput
 
-part2 :: String -> Integer
+part2 :: String -> Int
 part2 = solvePart 30000000 . parseInput
